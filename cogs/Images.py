c56109a600d4a216036438ae28d973aa2c5ab521
Spar0w <sbinvt1236@gmail.com>
2020-09-14 07:56:26 -0400

    Added monkey command

--- cogs/Images.py
+++ cogs/Images.py
@@ -1,8 +1,13 @@
 import discord
 from discord.ext import commands
 import aiohttp
+from google_images_download import google_images_download
+import json
+import random
 
 class Images(commands.Cog):
+
+    response = google_images_download.googleimagesdownload()
 
     def __init__(self, bot):
         self.bot = bot
@@ -16,6 +21,28 @@ class Images(commands.Cog):
                     await ctx.send(await img.text())
                 else:
                     await ctx.send("bot broke lmao")
+
+    #Pull a URL from google images of a monkey and post that url
+    @commands.command(pass_context=True)
+    async def monkey(self, ctx):
+        arguments = {"keywords":"awesome monkey","limit":100,"no_download":True}
+        try:
+            monkeys = self.response.download(arguments)
+        except:
+            print("lol")
+        #Convert the outputed tuple into a string
+        monkeys = str(monkeys)
+        try:
+            #Convert the string output to an array
+            monkey_list = monkeys[21:-5]
+            monkey_list = monkey_list.strip('][').split(', ')
+            #Get a random monkey image from the result
+            ran_num = random.randint(0, len(monkey_list))
+            monkey_pic = monkey_list[ran_num]
+            #Send the image
+            await ctx.send(monkey_pic[1:-1])
+        except IndexError:
+            await ctx.send("`Something went wrong lol`")
 
 def setup(bot):
     bot.add_cog(Images(bot))
