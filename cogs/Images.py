a425fb0c2c42de0c5488abbfe00105640dc210ff
Spar0w <sbinvt1236@gmail.com>
2020-09-19 18:38:58 -0400

    cleaned up the monkey command a bit and made it more random

--- cogs/Images.py
+++ cogs/Images.py
@@ -12,6 +12,27 @@ class Images(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def query_monkey(self):
+        monkey_kinds = ["monkeys", "cool monkeys", "monkey", "awesome monkey", "monkey in tree", "funny monkey"]
+        return random.choice(monkey_kinds)
+
+    #Pull a list of URLs from google images
+    async def find_monkey(self):
+        arguments = {"keywords":self.query_monkey(),"limit":100,"no_download":True}
+        try:
+            monkeys = self.response.download(arguments)
+        except:
+            print("lol")
+        #Convert the outputed tuple into a string
+        monkeys = str(monkeys)
+        try:
+            #Convert the string output to an array
+            monkey_list = monkeys[21:-5]
+            monkey_list = monkey_list.strip('][').split(', ')
+            return monkey_list
+        except IndexError:
+            await ctx.send("`Something went wrong lol`")
+
     #Loads from my personal gondola collection
     @commands.command(pass_context=True)
     async def gondola(self, ctx):
@@ -25,17 +46,8 @@ class Images(commands.Cog):
     #Pull a URL from google images of a monkey and post that url
     @commands.command(pass_context=True)
     async def monkey(self, ctx):
-        arguments = {"keywords":"awesome monkey","limit":100,"no_download":True}
         try:
-            monkeys = self.response.download(arguments)
-        except:
-            print("lol")
-        #Convert the outputed tuple into a string
-        monkeys = str(monkeys)
-        try:
-            #Convert the string output to an array
-            monkey_list = monkeys[21:-5]
-            monkey_list = monkey_list.strip('][').split(', ')
+            monkey_list = await self.find_monkey()
             #Get a random monkey image from the result
             ran_num = random.randint(0, len(monkey_list))
             monkey_pic = monkey_list[ran_num]
@@ -43,6 +55,8 @@ class Images(commands.Cog):
             await ctx.send(monkey_pic[1:-1])
         except IndexError:
             await ctx.send("`Something went wrong lol`")
+
+
 
 def setup(bot):
     bot.add_cog(Images(bot))
